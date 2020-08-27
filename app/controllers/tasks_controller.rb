740ba27370697b32125e7e6fc5ec1d0785670164
k <wakyaaa822@gmail.com>
2020-08-27 13:33:53 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks = Task.all
@@ -48,11 +48,11 @@ class TasksController < ApplicationController
   private
   
   def set_task
-    @task = task.find(params[:id])
+    @task = Task.find(params[:id])
   end
   
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
   
 end
